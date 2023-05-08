300cb33493d2415ff7b737665e3eaff39e020e24
niikhlsharma <65024567+niikhlsharma@users.noreply.github.com>
2023-05-08 19:32:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,20 @@ import App from "./components/App";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const shapeType = document.getElementById('shape-type');
+const addShapeBtn = document.getElementById('add-shape');
+const shapesHolder = document.getElementById('shapes-holder');
+
+addShapeBtn.addEventListener('click', function() {
+  const selectedShape = shapeType.value;
+  const newShape = document.createElement('div');
+  
+  if (selectedShape === 'square') {
+    newShape.classList.add('square');
+  } else if (selectedShape === 'circle') {
+    newShape.classList.add('circle');
+  }
+  
+  shapesHolder.appendChild(newShape);
+});
